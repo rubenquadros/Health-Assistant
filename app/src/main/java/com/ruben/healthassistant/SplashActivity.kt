f78fe4ba39100fb41e9b9986dc5b6e0a2bf44c65
rubenquadros <rquadros95@gmail.com>
2021-09-10 22:55:42 +0530

    Adding home screen

--- app/src/main/java/com/ruben/healthassistant/SplashActivity.kt
+++ app/src/main/java/com/ruben/healthassistant/SplashActivity.kt
@@ -1,6 +1,7 @@
 package com.ruben.healthassistant
 
 import android.annotation.SuppressLint
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
@@ -8,5 +9,6 @@ import android.os.Bundle
 class SplashActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        startActivity(Intent(this, HomeActivity::class.java))
     }
 }
