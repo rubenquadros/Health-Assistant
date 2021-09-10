8785d578d72a8e5e4cde2ec9ff774ec3ca0d13c2
rubenquadros <rquadros95@gmail.com>
2021-09-10 21:09:05 +0530

    initial commit

---
+++ app/src/main/java/com/ruben/healthassistant/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.ruben.healthassistant
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
